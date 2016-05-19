1fd28a8587de93d99001ae806a0b5c2c6d93741a
jinxing <jin_xing@aol.com>
2016-05-19 10:56:01 +0800

    Added authentication controller routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('/user/login', 'Auth\AuthController@postLogin');
+Route::post('/user/register', 'Auth\AuthController@postRegister');
